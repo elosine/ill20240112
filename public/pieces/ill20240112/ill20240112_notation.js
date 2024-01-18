09796cd3618367a8171484382779fe359a93fb82
elosine <justinwenloyang@gmail.com>
2024-01-18 09:46:20 -0500

    Update ill20240112_notation.js

--- public/pieces/ill20240112/ill20240112_notation.js
+++ public/pieces/ill20240112/ill20240112_notation.js
@@ -6,14 +6,29 @@ Scrolling Cursors
 
 //#ef General Variables
 const TEMPO_COLORS = [clr_limeGreen, clr_mustard, clr_brightBlue, clr_brightOrange, clr_lavander, clr_darkRed2, clr_brightGreen, clr_lightGrey, clr_neonMagenta, clr_plum, clr_blueGrey, clr_lightGrey, clr_lightGreen];
+//Dimensions
+const NOTATION_H = 100;
+const GAP_BTWN_NOTATION_LINES = 3;
+const VERT_DISTANCE_BETWEEN_LINES = NOTATION_H + GAP_BTWN_NOTATION_LINES;
+const NUM_NOTATION_LINES = 5;
+let WORLD_W = 945;
+let WORLD_H = (NOTATION_H * NUM_NOTATION_LINES) + (GAP_BTWN_NOTATION_LINES * (NUM_NOTATION_LINES - 1));
+//Timing
+let FRAMECOUNT = 0;
+const FRAMERATE = 60;
+const MS_PER_FRAME = 1000.0 / FRAMERATE;
+const PX_PER_BEAT = 70;
+const TOTAL_NUM_PX_IN_SCORE = WORLD_W * NUM_NOTATION_LINES;
+//Timesync
+const TS = timesync.create({
+  server: '/timesync',
+  interval: 1000
+});
 //#endef General Variables
 
 //#ef Animation Engine
 let cumulativeChangeBtwnFrames_MS = 0;
 let epochTimeOfLastFrame_MS;
-let FRAMECOUNT = 0;
-const FRAMERATE = 60;
-const MS_PER_FRAME = 1000.0 / FRAMERATE;
 
 function animationEngine(timestamp) {
   let ts_Date = new Date(TS.now());
@@ -30,7 +45,12 @@ function animationEngine(timestamp) {
 }
 
 function update() {
-
+  //Scrolling Cursors
+  totalNumFramesPerTempo.forEach((numFrames, tempoIx) => {
+    let currFrame = FRAMECOUNT % numFrames;
+    updateScrollingCsrs(currFrame, tempoIx);
+    updateBbs(currFrame, tempoIx);
+  });
 }
 //#endef Animation Engine
 
@@ -38,6 +58,8 @@ function update() {
 function init() { //runs from html file: ill20231212.html <body onload='init();'></body>
   makeCanvas();
   mkStaffRects();
+  makeScrollingCursors();
+  calcTimeline();
   //Initialize clock and start animation engine
   let ts_Date = new Date(TS.now()); //Date stamp object from TimeSync library
   let tsNowEpochTime_MS = ts_Date.getTime(); //current time at init in Epoch Time MS
@@ -47,15 +69,9 @@ function init() { //runs from html file: ill20231212.html <body onload='init();'
 //#endef INIT
 
 //#ef Canvas
-const NOTATION_H = 100;
-const GAP_BTWN_NOTATION_LINES = 3;
-const VERT_DISTANCE_BETWEEN_LINES = NOTATION_H + GAP_BTWN_NOTATION_LINES;
-const NUM_NOTATION_LINES = 5;
-let WORLD_W = 945;
-let WORLD_H = (NOTATION_H * NUM_NOTATION_LINES) + (GAP_BTWN_NOTATION_LINES * (NUM_NOTATION_LINES - 1));
-console.log(WORLD_H);
 let canvas = {};
 let panelTitle = "Interactive Looping Line 20240112";
+const staffRects = [];
 
 function makeCanvas() {
   let tPanel = mkPanel({
@@ -84,10 +100,7 @@ function makeCanvas() {
   tSvg.style.backgroundColor = clr_mustard;
   canvas['svg'] = tSvg;
 }
-//#endef Canvas
 
-//#ef Staff Rects
-const staffRects = [];
 function mkStaffRects() {
   for (var i = 0; i < NUM_NOTATION_LINES; i++) {
     let tRect = mkSvgRect({
@@ -104,7 +117,126 @@ function mkStaffRects() {
     staffRects.push(tRect);
   }
 }
-//#endef Staff Rects
+//#endef Canvas
+
+//#ef Scrolling Cursors
+let scrollingCursors = [];
+let scrCsrText = [];
+let scrollingCsrY1 = 30;
+let scrollingCsrH = 65;
+let scrollingCsrClrs = [];
+let lineY = [];
+for (var i = 0; i < NUM_NOTATION_LINES; i++) {
+  let ty = scrollingCsrY1 + ((NOTATION_H + GAP_BTWN_NOTATION_LINES) * i);
+  lineY.push(ty);
+}
+let tempos = [
+  [60, 60, ''],
+  [37.14, 37.14, ''],
+  [96.92, 37.14, 'd'],
+  [32.3, 86.67, 'a'],
+  [86.67, 86.67, '']
+];
+let totalNumFramesPerTempo = [];
+let tempoConsts = [];
+tempos.forEach((tempo, tix) => {
+  scrollingCsrClrs.push(TEMPO_COLORS[tix % TEMPO_COLORS.length]);
+});
+tempos.forEach((tempoArr, i) => {
+  let td = {};
+  //convert initial and final tempi from bpm to pixelsPerFrame
+  let iTempo = tempoArr[0]; //bpm
+  let fTempo = tempoArr[1]; //bpm
+  td['iTempoBPM'] = iTempo;
+  td['fTempoBPM'] = fTempo;
+  // convert bpm to pxPerFrame: pxPerMinute = iTempo * PX_PER_BEAT; pxPerSec = pxPerMinute/60; pxPerFrame = pxPerSec/FRAMERATE
+  let iTempoPxPerFrame = ((iTempo * PX_PER_BEAT) / 60) / FRAMERATE;
+  let fTempoPxPerFrame = ((fTempo * PX_PER_BEAT) / 60) / FRAMERATE;
+  td['iTempoPxPerFrame'] = iTempoPxPerFrame;
+  td['fTempoPxPerFrame'] = fTempoPxPerFrame;
+  //calc acceleration from initial tempo and final tempo
+  // a = (v2 - u2) / 2s ; v=finalVelocity, u=initialVelocity, s=totalDistance
+  let tAccel = (Math.pow(fTempoPxPerFrame, 2) - Math.pow(iTempoPxPerFrame, 2)) / (2 * TOTAL_NUM_PX_IN_SCORE);
+  // console.log('tempo ' + i + ' acceleration: ' + tAccel);
+  td['accel'] = tAccel;
+  // Calculate total number of frames from acceleration and distance
+  // t = sqrRoot( (2L/a) ) ; L is total pixels
+  let totalDurFrames;
+  if (tAccel == 0) {
+    totalDurFrames = Math.round(TOTAL_NUM_PX_IN_SCORE / iTempoPxPerFrame);
+  } else {
+    totalDurFrames = Math.round((fTempoPxPerFrame - iTempoPxPerFrame) / tAccel);
+  }
+  // console.log('Total Frames, tempo ' + i + ' : ' + totalDurFrames);
+  td['totalDurFrames'] = totalDurFrames;
+  tempoConsts.push(td);
+});
+
+function calcTimeline() {
+  tempoConsts.forEach((tempoObj, tempoIx) => { //run for each tempo
+    let frameArray = [];
+    let tNumFrames = Math.round(tempoObj.totalDurFrames); //create an array with and index for each frame in the piece per tempo
+    for (var frmIx = 0; frmIx < tNumFrames; frmIx++) { //loop for each frame in the piece
+      let td = {}; //dictionary to hold position values
+      //Calculate x
+      let tCurPx = Math.round((tempoObj.iTempoPxPerFrame * frmIx) + ((tempoObj.accel * Math.pow(frmIx, 2)) / 2));
+      td['absX'] = tCurPx;
+      let tx = tCurPx % NOTATION_LINE_LENGTH_PX; //calculate cursor x location at each frame for this tempo
+      td['x'] = tx;
+      //Calc Y pos
+      let tLineNum = Math.floor(tCurPx / NOTATION_LINE_LENGTH_PX)
+      let ty = scrollingCsrY1 + ((NOTATION_H + GAP_BTWN_NOTATION_LINES) * tLineNum);
+      td['y'] = ty;
+      frameArray.push(td);
+    }
+    tempoConsts[tempoIx]['frameArray'] = frameArray;
+    totalNumFramesPerTempo.push(frameArray.length);
+  });
+}
+
+function makeScrollingCursors() {
+  for (var i = 0; i < tempos.length; i++) {
+    let tCsr = mkSvgLine({
+      svgContainer: canvas.svg,
+      x1: 0,
+      y1: scrollingCsrY1,
+      x2: 0,
+      y2: scrollingCsrY1 + scrollingCsrH,
+      stroke: scrollingCsrClrs[i],
+      strokeW: 2
+    });
+    tCsr.setAttributeNS(null, 'stroke-linecap', 'round');
+    tCsr.setAttributeNS(null, 'display', 'yes');
+    scrollingCursors.push(tCsr);
+    //Cursor Text
+    let tTxt = mkSvgText({
+      svgContainer: canvas.svg,
+      x: 0,
+      y: scrollingCsrY1,
+      fill: scrollingCsrClrs[i],
+      stroke: scrollingCsrClrs[i],
+      strokeW: 1,
+      justifyH: 'start',
+      justifyV: 'auto',
+      fontSz: 18,
+      fontFamily: 'lato',
+      txt: tempos[i][2]
+    });
+    scrCsrText.push(tTxt);
+  }
+}
+
+function updateScrollingCsrs(frame, tempoIx) {
+  let tx = tempoConsts[tempoIx].frameArray[frame].x;
+  let ty = tempoConsts[tempoIx].frameArray[frame].y;
+  scrollingCursors[tempoIx].setAttributeNS(null, 'x1', tx);
+  scrollingCursors[tempoIx].setAttributeNS(null, 'x2', tx);
+  scrollingCursors[tempoIx].setAttributeNS(null, 'y1', ty);
+  scrollingCursors[tempoIx].setAttributeNS(null, 'y2', ty + scrollingCsrH);
+  scrCsrText[tempoIx].setAttributeNS(null, 'x', tx - 5);
+  scrCsrText[tempoIx].setAttributeNS(null, 'y', ty - 2);
+}
+//#endef Scrolling Cursors
 
 
 
